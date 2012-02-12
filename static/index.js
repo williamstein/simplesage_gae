21508a8adc55b40369191de48a88f100d7661722
Rado Kirov <radokirov@google.com>
2012-02-12 05:46:16 -0800

    js history implemented.

--- static/index.js
+++ static/index.js
@@ -15,7 +15,11 @@ sendAjaxJsonMessage = function(path, opt_object, opt_callback, opt_this) {
   });
 };
 
-Controller = function() {};
+Controller = function() {
+  this.history = [];
+  this.cur_history = -1;
+  this.history_has_unevaled = false;
+};
 
 Controller.prototype.send_input = function() {
   this.input = this.input_codemirror.getValue();
@@ -29,6 +33,8 @@ Controller.prototype.send_input = function() {
 Controller.prototype.ack = function(message) {
   if (message.status == 'ok') {
     this.extend_output(this.input);
+    this.update_history(this.input);
+    this.input = '';
   } else {
     alert(message.status);
     this.undo();
@@ -51,6 +57,34 @@ Controller.prototype.undo = function() {
   this.input_codemirror.setValue(this.input);
   this.input = '';
 };
+
+Controller.prototype.up_arrow = function(message) {
+  if (this.cur_history < 0) return;
+  if (!this.history_has_unevaled) {
+    this.history.push(this.input_codemirror.getValue());
+    this.history_has_unevaled = true;
+  }
+  this.input_codemirror.setValue(this.history[this.cur_history]);
+  this.cur_history -= 1;
+}
+
+Controller.prototype.update_history = function(text) {
+  if (this.history_has_unevaled) this.history.pop();
+  this.history.push(text);
+  this.history_has_unevaled = false;
+  this.cur_history = this.history.length - 1;
+}
+
+Controller.prototype.down_arrow = function(message) {
+  if (this.cur_history + 2 >= this.history.length) return;
+  this.input_codemirror.setValue(this.history[this.cur_history + 2]);
+  if (this.cur_history + 2 >= this.history.length - 1
+      && this.history_has_unevaled) {
+    this.history_has_unevaled = false;
+    this.history.pop();
+  }
+  this.cur_history += 1;
+}
 
 establishChannel = function(token) {
   if (token === undefined) {
@@ -86,7 +120,7 @@ establishChannel = function(token) {
   socket.onclose = onClose;
 }
 
-onload = function() {  
+onload = function() {
   controller = new Controller();
 
   var output = CodeMirror.fromTextArea(document.getElementById("output2"), {
@@ -106,6 +140,18 @@ onload = function() {
               return true;
           }
           if (e.keyCode === 13 && e.type === 'keyup' && e.shiftKey) {
+              e.stop();
+              return true;
+          }
+          if (e.keyCode === 40 && e.type === 'keydown' &&
+              editor.getCursor().line === editor.lineCount() - 1) {
+              controller.down_arrow();
+              e.stop();
+              return true;
+          }
+          if (e.keyCode === 38 && e.type === 'keydown' &&
+              editor.getCursor().line === 0) {
+              controller.up_arrow();
               e.stop();
               return true;
           }
